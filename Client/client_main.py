dae4f81e862403b1318303be5ffd170b041be1ff
HacktonCRT <44427613+HacktonCRT@users.noreply.github.com>
2018-10-24 06:40:17 -0700

    insert recorder

--- Client/client_main.py
+++ Client/client_main.py
@@ -1,1 +1,13 @@
-import 
+import requests
+from Recorder import TimeRecorder
+
+if __name__ == "__main__":
+    for i in range(1,3):
+        #fileName = str(time.time())+".wav"
+        fileName = "deen_" +str(i) + ".wav"
+        TimeRecorder(fileName).Record()
+    exit(0)
+    #send http
+    with open(fileName, 'rb') as f:
+        r = requests.post('http://httpbin.org/post', files={fileName: f})
+        print r.content
